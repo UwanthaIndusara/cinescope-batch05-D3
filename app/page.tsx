9a799ff42b9fda261543cbf614cb72bee16ef833
Uwantha Indusara <uwantha2021@gmail.com>
2025-11-28 02:33:19 +0530

    Add shadcn initial config

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ export default function Home() {
   const myVariable = "Hello world";
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div 
+      className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"
+      style={{ backgroundColor: "blue" }} 
+    >
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
